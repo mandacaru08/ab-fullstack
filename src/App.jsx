972b51589aa6a1599b931180761875abd29a0c99
Jansenck <caik11@hotmail.com>
2023-05-25 10:26:18 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+import TopBar from './components/TopBar';
+import Destination from './components/Destination';
+import BoatOffersComparator from './components/BoatOffersComparator';
+import Marketing from './components/Marketing';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <Container>
+        
+      </Container>
+    </>
+  )
+}
+
+export default App;
+
+const Container = styled.div`
+  padding:0 15%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media(max-width: 1200px){
+    padding: 0 10%;
+  }
+  @media(max-width: 1080px){
+    padding: 0 5%
+  }
+
+  @media(max-width: 770px){
+    padding: 0 0;
+  }
+`;
