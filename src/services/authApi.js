90c3ad288c45e191a179097a2ca72510b8fb1499
jansencaik <jansencaik@gmail.com>
2023-12-04 16:29:05 -0300

    Fix: change function args

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -1,11 +1,11 @@
 import api from './api';
 
-export async function signIn(email, password) {
-    const response = await api.post(`${import.meta.env.VITE_API_BASE_URL}/api/user/sign-in`, {email, password: password});
+export async function signIn(user) {
+    const response = await api.post(`${import.meta.env.VITE_API_BASE_URL}/api/user/sign-in`, {email: user.email, password: user.password});
     return response.data;
 };
 
-export async function signUp(email, password) {
-    const response = await api.post(`${import.meta.env.VITE_API_BASE_URL}/api/user/sign-up`, {email, password: password});
+export async function signUp(user) {
+    const response = await api.post(`${import.meta.env.VITE_API_BASE_URL}/api/user/sign-up`, {email: user.email, password: user.password});
     return response.data;
 };
