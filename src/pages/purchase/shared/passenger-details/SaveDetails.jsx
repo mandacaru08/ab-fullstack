5c96daf42bc36b7e8a453733a4af7289bdd275bf
jansencaik <jansencaik@gmail.com>
2023-12-20 16:40:15 -0300

    refact: add reusable checkbox input

--- src/pages/purchase/shared/passenger-details/SaveDetails.jsx
+++ src/pages/purchase/shared/passenger-details/SaveDetails.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-
 import styled from "styled-components";
 import { BsFillCheckCircleFill, BsFillCheckSquareFill } from "react-icons/bs";
 import { ImInfo } from "react-icons/im";
 import Button from "../../../../components/Button";
 import { useNavigate } from "react-router-dom";
+import Input from "../../../../components/Input";
 
 const advantages = [
   {
@@ -37,20 +37,19 @@ export default function SaveDetails() {
 
   return (
     <Container>
-      <label onChange={() => handleInputChange()}>
-        <InputContainer showInput={!saveDetails}>
-          <input type="checkbox" readOnly />
-          {saveDetails == true && <BsFillCheckSquareFill />}
-        </InputContainer>
-        <Title checked={saveDetails}>
-          <h4>Salve os detalhes para futuras reservas</h4>
-          <ImInfo />
-        </Title>
+      <section >
+        <Input 
+          type="checkbox"
+          inputVariant="checkbox"
+          label="Salve os detalhes para futuras reservas"
+          onChange={() => handleInputChange()}
+          icon={saveDetails && <BsFillCheckSquareFill />}
+        />
         <Message>
           Se você deseja criar uma conta de cliente agora, poderá atribuir
           <br></br> seus dados de acesso na próxima etapa.
         </Message>
-      </label>
+      </section>
       <Advantages>
         <ol>
           {advantages.map((advantage) => {
@@ -87,51 +86,16 @@ const Container = styled.div`
   border-top: 1px solid #f0f3f5;
   border-radius: 0 0 4px 4px !important;
 
-  label {
+  section {
     height: 98px;
     width: 60%;
     padding: 16px 50px 16px 60px;
     box-sizing: border-box;
     position: relative;
-  }
-`;
-
-const InputContainer = styled.div`
-  height: 16px;
-  width: 16px;
-  position: absolute;
-  top: 40%;
-  left: 15px;
-  transform: translate(-50% -50%);
-  border-radius: 3px;
-
-  input {
-    height: 16px;
-    width: 16px;
-    border-radius: 3px;
-    border: 1px solid #858585;
-    pointer-events: none;
-    display: ${(props) => (props.showInput ? "block" : "none")};
-    margin: 0;
-  }
-`;
-
-const Title = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: left;
-  align-items: center;
-
-  h4 {
-    font-size: 16px;
-    font-weight: 700;
-  }
-
-  svg {
-    font-size: 20px;
-    font-weight: 700;
-    color: #8b8b8b;
-    margin-left: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: start;
   }
 `;
 
