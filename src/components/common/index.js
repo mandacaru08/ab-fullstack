83c3e3efc2c8022fcbfe3e6079dbd03bfbd53891
jansencaik <jansencaik@gmail.com>
2024-02-15 19:45:02 -0300

    refact: export components as default from index

---
+++ src/components/common/index.js
@@ -0,0 +1,5 @@
+export { default as Footer } from './Footer';
+export { default as Logo } from './Logo';   
+export { default as Marketing } from './Marketing';
+export { default as GeneralInfos } from './GeneralInfos';
+export { default as BoatOffersComparator } from './BoatOffersComparator';
